ccb51440185d6eff529dc34e7669d2f96a780b59
Rodrigo Lozada <rlozada1997@gmail.com>
2025-04-08 00:54:48 -0400

    jest fixes

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,15 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.config.js',
+  ],
+  coverageReporters: ['text', 'lcov'],
+  verbose: true
 };
