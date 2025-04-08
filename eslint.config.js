e7f242eb5d81a8dc62ee904f73914d6b688323b1
Rodrigo Lozada <rlozada1997@gmail.com>
2025-04-08 00:15:08 -0400

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,28 @@
-﻿module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-native/all',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', 'react-native', '@typescript-eslint'],
-  rules: {
-    // Customize rules as needed
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'react-native/no-inline-styles': 'warn'
+﻿const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
+
+module.exports = [
+  ...compat.extends('expo'),
+  
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+    },
   },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
-};
+  
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.expo/',
+      'web-build/',
+    ],
+  },
+];
