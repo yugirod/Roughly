243da1b2207331202e4145483f266b5027063ae4
Rodrigo Lozada <rlozada1997@gmail.com>
2025-04-08 00:07:56 -0400

    eslint update

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  rules: {
+    // Customize rules as needed
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-native/no-inline-styles': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
